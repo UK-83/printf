18f6cc67be65b15e81ae1308bb3003415758b6e7
tracy ukpebor <tracymarvel83@gmail.com>
2023-06-26 03:24:58 -0700

    updated

--- functions.c
+++ functions.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 /************************* PRINT CHAR *************************/
 /**
  * print_char - Prints a char
--- get_width.c
+++ get_width.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * get_width - Calculates the width for printing
  * @format: Formatted string in which to print the arguments.
@@ -31,5 +32,3 @@ int get_width(const char *format, int *i, va_list list)
 	*i = curr_i - 1;
 	return (width);
 }
-
-
